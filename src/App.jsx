b4f2cfeddfa162dfe46e1dff6c8477bddb4b0790
vasanthkrishnan <vasanthakrishnans22it@srishakthi.ac.in>
2025-04-18 11:24:56 +0530

    created-header

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from "./component/Header";
 
 export const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element = {<Home />} />
-        <Route path="/sing-in" element = {<SignIn />} />
-        <Route path="/sing-out" element = {<SignUp />} />
+        <Route path="/sign-in" element = {<SignIn />} />
+        <Route path="/sing-up" element = {<SignUp />} />
         <Route path="/profile" element = {<Profile />} />
         <Route path="/about" element = {<About />} />
       </Routes>
